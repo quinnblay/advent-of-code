50fda2005137ac94a423448c1d0467c68a2a5009
Quinn Blay <quinn.blay@gmail.com>
2015-12-19 15:16:13 +1100

    Day 5 - Solved
    Part 2 - 51

--- solutions/day_5/part_2_solution.py
+++ solutions/day_5/part_2_solution.py
@@ -2,13 +2,19 @@ import sys
 import re
 
 # pattern 1 - at least 3 vowels (aeiou) - check for 3x later
-p_vowels = re.compile('[aeiou]')
+#p_vowels = re.compile('[aeiou]')
 
 # pattern 2 - a letter appears twice in a row
-p_row = re.compile('(\\w)\\1')
+#p_row = re.compile('(\\w)\\1')
 
 # pattern 3 - none of these: ab, cd, pq, or xy
-p_strings = re.compile('(ab)|(cd)|(pq)|(xy)')
+#p_strings = re.compile('(ab)|(cd)|(pq)|(xy)')
+
+# pattern 1 - pair of 2 letters > appears twice
+p_pairs = re.compile('(..).*\\1')
+
+# pattern 2 - one letter that repeats with another in between
+p_repeats = re.compile('(.).\\1')
 
 # read the input file
 with open(sys.argv[1], 'r') as input_file:
@@ -17,16 +23,12 @@ with open(sys.argv[1], 'r') as input_file:
     # for every line in the input file
     for line in input_file:
         # check for at least 3 vowels
-        if len(p_vowels.findall(line)) < 3:
+        if len(p_pairs.findall(line)) < 1:
             continue  # failed first test
 
         # check for a letter twice in a row
-        if len(p_row.findall(line)) < 1:
+        if len(p_repeats.findall(line)) < 1:
             continue  # failed second test
-
-        # check for disallowed strings
-        if len(p_strings.findall(line)) > 0:
-            continue  # failed third test
 
         nice_count += 1
 
